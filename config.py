dc026845f4b0aaaad4277cb8db163d4bbe1e6b25
Faith <jamienibtong@gmail.com>
2019-06-14 15:57:20 +0800

    updated token

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """Change the line in quotation marks to your own bot token."""
-token = "NDYwOTk4MDg5Mzk5MDc0ODE2.D1J1kw.JM8B2I77VpScvOGeHt23uPOUkoI"
+token = "NDYwOTk4MDg5Mzk5MDc0ODE2.XQNTFg.PRFi9m5EALXsKTK6jhO10cYboQw"
 music_enabled = True
 debug_enabled = True
 moderation_enabled = True
