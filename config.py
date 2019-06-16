3ff8e201b5fed607a79d014286e4599e97403255
anarchy <jamienibtong@gmail.com>
2019-06-16 08:51:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 """Change the line in quotation marks to your own bot token."""
-token = "NDYwOTk4MDg5Mzk5MDc0ODE2.D1J1kw.JM8B2I77VpScvOGeHt23uPOUkoI"
+import os
+
+token = os.environ.get('DISCORD_TOKEN')
 music_enabled = True
 debug_enabled = True
 moderation_enabled = True
