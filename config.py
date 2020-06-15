662fde4bb273e4398ad81eb56b619396fa1b347c
faith <jamienibtong@gmail.com>
2020-06-15 20:54:32 +0800

    remove config.toml

---
+++ config.py
@@ -0,0 +1,13 @@
+'''Change the line in quotation marks to your own bot token.'''
+
+Config = {
+    'token': 'NDYwOTk4MDg5Mzk5MDc0ODE2.XpHHDw.OsZP97fjA27psgAi9kueZrm82k4',
+    'prefix': 'waf ',
+    'extensions': ['music', 'debug', 'moderation', 'errors'],
+    'autorole': 'Member',
+    'dj': None,
+    'log_channel': 'mod-log',
+    'welcome_channel': None,
+    'mute': 'Edgelord',
+    'queue_capacity': 50
+}
---
+++ waffle/config.py
@@ -0,0 +1,18 @@
+import waffle.database
+
+'''Change the line in quotation marks to your own bot token.'''
+
+config = {
+    'token': 'NDYwOTk4MDg5Mzk5MDc0ODE2.Xm91PQ.Sy7X6SGwrvvaKwKW0G1D7G96yUM',
+    'prefix': 'waf ',
+    'extensions': ['music', 'debug', 'moderation', 'errors'],
+    'autorole': 'Member',
+    # Set this to None if you do not want autorole
+    'dj': None,
+    # Set this to None if you do not want DJ functionality
+    'log_channel': 'mod-log',
+    'welcome_channel': None,
+    # Set this to None if you do not want welcome messages
+    'mute': 'Edgelord'
+    # Set this to None if you want to disable muting
+}
