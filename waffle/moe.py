6260ff90043dd73a433af1101ad567e43760869c
faith <jamienibtong@gmail.com>
2021-04-13 12:43:12 +0800

    added test command for moe cog

--- waffle/moe.py
+++ waffle/moe.py
@@ -1,4 +1,5 @@
-from jikanpy import AioJikan
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
 
 import discord
 from discord.ext import commands
@@ -12,12 +13,35 @@ def setup(bot):
 class Moe(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.transport = AIOHTTPTransport(url="https://graphql.anilist.co")
+
+    async def query_anime(self):
+        pass
 
     @commands.group(name="moe", invoke_without_command=True)
     @commands.guild_only()
-    async def moe(self, ctx):
+    async def moe(self, ctx, id):
         """
-        Group for Anime-related command.
+        Group for Anime-related commands.
         """
-        pass
 
+        client = Client(transport=self.transport, fetch_schema_from_transport=True)
+        params = {"id": id}
+        query = gql(
+            """
+            query ($id: ID){
+              hero {
+                id
+                name
+                friends {
+                  name
+                }
+              }
+            }
+            """
+        )
+
+        async with client as session:
+            result = await session.execute(query, variable_values=params)
+
+        await ctx.send(result)
